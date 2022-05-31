8d684361349a6cfa299d338ff8d5ac2d0351acd7
matthewAbianco <matthewbianco@hotmail.com>
2022-05-30 23:23:14 -0400

    finished creating tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,19 +1,23 @@
-// __tests__/About.test.js
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
-import About from '..';
+import About from '..'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
 describe('About component', () => {
-  it('renders', () => {
-    render(<About />);
-  });
-  
-  it('matches snapshot DOM node structure', () => {
-    const { asFragment } = render(<About />);
-    
-    expect(asFragment()).toMatchSnapshot();
-  });
+    //first test
+
+    it('renders', () => {
+        render(<About />)
+    })
+
+    //second test
+
+    it('matches snapshot DOM node structure', () => {
+        //render about
+        const { asFragment } = render(<About />)
+        expect(asFragment()).toMatchSnapshot();
+    })
+
 })
---
+++ src/components/Gallery/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Gallery = () => {
+    return (
+        <div>
+            <h1> Name </h1>
+            <p> Description</p>
+        </div>
+    )
+}
+
+export default Gallery
