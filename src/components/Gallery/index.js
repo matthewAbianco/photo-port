2a4c5ab4408705ac6df94003cf014e64f2ee3105
matthewAbianco <matthewbianco@hotmail.com>
2022-05-31 01:28:39 -0400

    moved code between documents

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { capitalizeFirstLetter } from '../../utils/helpers'
+import photo from '../../assets/small/commercial/0.jpg'
 
-const Gallery = () => {
+const Gallery = (props) => {
+
+    const currentCategory = {
+        name: 'commercial',
+        description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    }
+
     return (
         <div>
-            <h1> Name </h1>
-            <p> Description</p>
+            <h1> {capitalizeFirstLetter(currentCategory.name)} </h1>
+            <p> {currentCategory.name} </p>
+            <div className='flex-row' >
+                <img className='img-thumbnail mx-1' src={photo} alt="" />
+            </div>
         </div>
     )
 }
